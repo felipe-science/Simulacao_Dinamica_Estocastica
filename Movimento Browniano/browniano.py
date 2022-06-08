ce3a13df58aeac7e8eb51eb8b8757633715b385b
felipemoreira <felipemoreira.ufrn@gmail.com>
2022-06-08 11:02:47 -0300

    alteracao

--- Movimento Browniano/browniano.py
+++ Movimento Browniano/browniano.py
@@ -1,5 +1,6 @@
 import random
 from numpy import pi, zeros, sqrt
+from scipy.stats import norm
 from pylab import plot, show, title, xlabel, ylabel, tick_params, legend
 
 def caminhada_aleatoria(n_passos=1000):
@@ -10,10 +11,11 @@ def caminhada_aleatoria(n_passos=1000):
         w[i] = w[i-1]+yi
     return w
 
-processo1 = caminhada_aleatoria()
-processo2 = caminhada_aleatoria()
-processo3 = caminhada_aleatoria()
-#plot(processo1, processo2)
+
+
+processo1 = caminhada_dist_normal()
+processo2 = caminhada_dist_normal()
+processo3 = caminhada_dist_normal()
 plot(processo1, label='Partícula 1')
 plot(processo2, label='Partícula 2')
 plot(processo3, label='Partícula 3')
