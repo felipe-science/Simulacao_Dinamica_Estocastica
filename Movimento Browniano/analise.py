f2caedbe5e99a6ff4b7de5f1484098070a8d9fd7
felipemoreira <felipemoreira.ufrn@gmail.com>
2022-06-11 15:37:21 -0300

    atualizacao 11/junho/2022

--- Movimento Browniano/analise.py
+++ Movimento Browniano/analise.py
@@ -1,26 +1,9 @@
 from numpy import loadtxt, linspace
-from pylab import scatter, plot, show, xlabel, ylabel
-
-livre_caminho_medio = 1e-9
-tempo_de_relaxacao = 1e-9
-
-
-dados = loadtxt('media_posicao.txt',float)
-
-posicaox1 = dados[:,0]
-posicaox2 = dados[:,1]
-posicaoy1 = dados[:,2]
-posicaoy2 = dados[:,3]
-tempo = linspace(0,len(posicaox1),len(posicaox1))
+from pylab import scatter, plot, show, xlabel, ylabel, hist
 
 
 
-
-plot(posicaox1)
-xlabel("Tempo [ns]")
-ylabel("<x> [nm]")
-
-show()
-
-scatter(tempo,posicaox1)
+# Média dados
+data = loadtxt("Npasso_Mposicao.txt")
+plot(data)
 show()
--- Movimento Browniano/browniano.py
+++ Movimento Browniano/browniano.py
@@ -4,7 +4,7 @@ from scipy.stats import norm
 from statistics import mean
 from pylab import plot, show, title, xlabel, ylabel, tick_params, legend, hist
 
-def caminhada_aleatoria(n_passos=100):
+def caminhada_aleatoria(n_passos=1000):
 
     w = zeros(n_passos)
     for i in range(1,n_passos):
@@ -17,17 +17,17 @@ def gerar_3caminhos():
     processo1 = caminhada_aleatoria()
     processo2 = caminhada_aleatoria()
     processo3 = caminhada_aleatoria()
-    
+
     plot(processo1, label='Partícula 1')
     plot(processo2, label='Partícula 2')
     plot(processo3, label='Partícula 3')
-    
+
     title("Caminhada aleatória unidimensional", fontsize='20')
     xlabel("Número de passos", fontsize='15')
     ylabel("Posição da partícula", fontsize = '15')
     tick_params(labelsize=13)
     legend()
-    show()     
+    show()
 
 
 def gerar_Ncaminhos():
